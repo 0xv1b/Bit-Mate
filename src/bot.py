f8d7dc281fcfe6b3f2de2ccbd6f011f9821fb7c0
Vibhor Singh <vibdaddy12@gmail.com>
2022-08-25 10:08:14 +0200

    Refactored position calculation

--- src/bot.py
+++ src/bot.py
@@ -9,22 +9,22 @@ class Bot():
     
     def runPVP(self):
         info = self._config._loadConfig()
-        pvp = (info["positions"]["pvp"][0] + info["screen"]["origin"][0], info["positions"]["pvp"][1] + info["screen"]["origin"][1])
+        pvp = self.calculatePosition(info["positions"]["pvp"])
         print(pvp)
-        pvp_play = (info["positions"]["pvp_play"][0] + info["screen"]["origin"][0], info["positions"]["pvp_play"][1] + info["screen"]["origin"][1])
-        pvp_fight = (info["positions"]["pvp_fight"][0] + info["screen"]["origin"][0], info["positions"]["pvp_fight"][1] + info["screen"]["origin"][1],)
-        pvp_accept = (info["positions"]["pvp_accept"][0] + info["screen"]["origin"][0], info["positions"]["pvp_accept"][1] + info["screen"]["origin"][1])
-        pvp_town = (info["positions"]["pvp_town"][0] + info["screen"]["origin"][0], info["positions"]["pvp_town"][1] + info["screen"]["origin"][1])
-        pvp_exit = (info["positions"]["pvp_exit"][0] + info["screen"]["origin"][0], info["positions"]["pvp_exit"][1] + info["screen"]["origin"][1])
+        pvp_play = self.calculatePosition(info["positions"]["pvp_play"])
+        pvp_fight = self.calculatePosition(info["positions"]["pvp_fight"])
+        pvp_accept = self.calculatePosition(info["positions"]["pvp_accept"])
+        pvp_town = self.calculatePosition(info["positions"]["pvp_town"])
+        pvp_exit = self.calculatePosition(info["positions"]["pvp_exit"])
         pvp_duration = info["duration"]["pvp"]
 
 
-        Timer(1, task = pyautogui.click, args = pvp).start()
-        Timer(2, task = pyautogui.click, args = pvp_play).start()
-        Timer(3, task = pyautogui.click, args = pvp_fight).start()
-        Timer(4, task = pyautogui.click, args = pvp_accept).start()
-        Timer(pvp_duration + 5, task = pyautogui.click, args = pvp_town).start()
-        Timer(25, task = pyautogui.click, args = pvp_exit).start()
+        Timer(1, pyautogui.click, args = pvp).start()
+        Timer(2, pyautogui.click, args = pvp_play).start()
+        Timer(3, pyautogui.click, args = pvp_fight).start()
+        Timer(4, pyautogui.click, args = pvp_accept).start()
+        Timer(pvp_duration + 5, pyautogui.click, args = pvp_town).start()
+        Timer(25, pyautogui.click, args = pvp_exit).start()
 
     def runWorldBoss(self):
         pass
@@ -43,3 +43,8 @@ class Bot():
 
     def runGauntlet(self):
         pass
+
+    # Adds the origin from the distance from the origin to get the exact screen coordinates
+    def calculatePosition(self, position):
+        info = self._config._loadConfig()
+        return (position[0] + info["screen"]["origin"][0], position[1] + info["screen"]["origin"][1])
